c22abb1c76c254bdd0fa589a438a01275e1ab414
Niroj Thapa <you@niroj.com>
2026-03-05 09:37:15 +0545

    fixed prisma generate

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pkg from "pg";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const { Pool } = pkg;
 
-export const prisma =
+// Neon connection
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+export const db =
   globalForPrisma.prisma ??
-  new PrismaClient();
+  new PrismaClient({
+    adapter, 
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
