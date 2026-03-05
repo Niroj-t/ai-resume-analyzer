08e57eed4a676004d915e56d03451a455bc38719
Niroj Thapa <you@niroj.com>
2026-03-05 10:24:08 +0545

    fixed db.ts isssue

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pkg from "pg";
-
-const { Pool } = pkg;
-
-// Neon connection
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter, 
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
