fbc926acf3855dca810f74917e413e7eaa4b52bd
Niroj Thapa <you@niroj.com>
2026-03-05 09:42:51 +0545

    fixed db.ts isssue

--- lib/db.ts
+++ lib/db.ts
@@ -14,6 +14,10 @@ export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter, 
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
